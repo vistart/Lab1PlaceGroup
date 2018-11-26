549fafd93ffea649b4106c4ff7e9b576a5b412a2
Zhao vistart <i@vistart.me>
2018-11-26 11:32:52 +0800

    Initial Commit.

--- Lab1PlaceGroup/Properties/AssemblyInfo.cs
+++ Lab1PlaceGroup/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Lab1PlaceGroup")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © vistart 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
